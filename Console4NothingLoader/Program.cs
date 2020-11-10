8a539cbd5be0b3586aaf50f6e8768dc2bc719278
Nekromateion <nekromateion@web.de>
2020-11-10 12:44:32 +0100

    Update Program.cs

--- Console4NothingLoader/Program.cs
+++ Console4NothingLoader/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Console4NothingLoader
+﻿using System.Reflection;
+
+namespace Console4NothingLoader
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Assembly
         }
     }
 }
