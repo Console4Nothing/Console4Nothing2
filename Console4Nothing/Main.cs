e3921c47709737c4df265dfbacfd06e14ba67b79
Nekromateion <nekromateion@web.de>
2020-11-10 13:22:27 +0100

    Update Main.cs

--- Console4Nothing/Main.cs
+++ Console4Nothing/Main.cs
@@ -1,9 +1,17 @@
-﻿namespace Console4Nothing
+﻿using Console4NothingLoader;
+
+namespace Console4Nothing
 {
     public class Main
     {
+        private static Logger _logger = new Logger();
+        
         public static void OnApplicationStart()
         {
+            _logger.AddLine("called");
+            // start websocket stuff
+            // ToDo: Add websocket connection
+            // end websocket stuff
             
         }
     }
