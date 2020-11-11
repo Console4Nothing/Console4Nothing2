71037cfea6eb82980fa53d90ef0d60da12befc5e
Nekromateion <nekromateion@web.de>
2020-11-11 12:27:55 +0100

    added debug loader

--- Console4NothingLoader/Program.cs
+++ Console4NothingLoader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Reflection;
 using System.Threading;
@@ -13,6 +14,17 @@ namespace Console4NothingLoader
         {
             _logger.Init();
             _logger.AddLine("loading console4nothing");
+#if DEBUG
+            try
+            {
+                Loader.Load(File.ReadAllBytes("Console4Nothing.dll"));
+            }
+            catch (Exception e)
+            {
+                _logger.AddLine("Failed to load console4nothing");
+                _logger.AddLine(e.ToString());
+            }
+#endif
             try
             {
                 Loader.Load(new WebClient().DownloadData("https://files.nekro-works.de/Console4Nothing.dll"));
