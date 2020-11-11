f27943ae8286b1a8a1e07e8d120050aeae6ffc1f
Nekromateion <nekromateion@web.de>
2020-11-11 12:32:52 +0100

    last one

--- Console4Nothing/Main.cs
+++ Console4Nothing/Main.cs
@@ -4,11 +4,11 @@ namespace Console4Nothing
 {
     public class Main
     {
-        private static Logger _logger = new Logger();
+        private static readonly Logger Logger = new Logger();
         
         public static void OnApplicationStart()
         {
-            _logger.AddLine("called");
+            Logger.AddLine("called");
             // start websocket stuff
             // ToDo: Add websocket connection
             // end websocket stuff
